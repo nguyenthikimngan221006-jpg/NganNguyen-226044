29a9d2d9f877c0ea1a63a8c64d123426aefac54f
Ngan06 <nguyenthikimngan221006@gmail.com>
2026-08-23 16:45:59 +0700

    buoi 2

--- Project/main.c
+++ Project/main.c
@@ -1,9 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 void main()
 {
-	printf("Hello world!");
+	//int n = 0;
+	//printf("Nhap gia tri tu ban phim: ");
+	//	scanf("%d", &n);
+	//printf("Gia tri vua nhap la: %d\n", n);
+	
 
-	printf("Hello C");
+//Bai 1 nhap 
 
-	printf("Hello C++");
+	//printf("Nhap gia tri tu ban phim: ");
+	//scanf("%d", &n);
+	//printf("He 10: %d\n", n);
+	//printf("He 16: %x\n", n);
+	  
+
+
+
+// bai 2 yeu cau user nhap vao mssv va diem float 
+	// in mssv va diem ra man hinh
+	//float m=0 ;
+	//printf("Nhap mssv: ");
+	//scanf("%d", &n);
+	//printf("Nhap diem: ");
+	//scanf("%f", &m);
+	//printf("Mssv cua ban la: %d, Diem cua ban la:%0.1f\n", n,m);
+//Bai 3 yeu cau nhap vao hai so nguyen a,b 
+	//in ra tong hieu tich thuong cho moi dong
+
+	//int a = 0;
+	//int b = 0;
+
+	//printf("Nhap so nguyen a: ");
+	//scanf("%d", &a);
+	//printf("Nhap so nguyen b: ");
+	//scanf("%d", &b);
+
+	//printf("Tong:%d, Hieu:%d, Tich:%d, Thuong:%0.1f, Du:%d\n", a + b, a - b, a * b, (float)(a) / b, a % b);
+
+//Bai 4 Nhap nhiet do c(so nguyen) va doi sang nhiet do f
+//in ra manhinh do f (so thuc)
+//F = c*9/5+3
+	//int c = 0;
+	//printf("Nhap nhiet do c: ");
+	//scanf("%d", &c);
+	//printf("Nhiet do f la:%f\n",(float)c * 9/5 + 32);
+// Bài 5 Yêu cầu user nhập vào số giây (số nguyên)
+// in ra số giờ số phứt số giây trên mỗi dòng
+	//int g = 0;
+	//printf("Hay nhap vao so giay: ");
+	//scanf("%d" ,&g);
+	//printf("So gio: %d\n", g / 3600); g = g % 3600;
+	//printf("So phut: %d\n", g / 60); g = g % 60;
+	////printf("So giay: %d\n",g);
+// Bài 6 Nhập vào bán kính r 
+// In ra chu vi, diện tích của hình tròn bán kính r đó trên mỗi dòng
+// chu vi =2r*3.14
+// diện tích =r*r*3.14
+	int r = 0;
+	printf("Nhap vao ban kinh r: ");
+	scanf("%d", &r);
+	printf("Chu vi cua hinh tron la:%f,Dien tich cua hinh tron la: %f\n",2*r*3.14,r*r*3.14);
+
+
+				
+	
+
 }
